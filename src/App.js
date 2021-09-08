8c86b2c113b0df3d60433fdf7dd0fb74313a4898
JuanHerrera8Pc <herrera.francojdr@gmail.com>
2021-09-08 18:16:16 -0500

    Añadi carpeta services (la movi de sitio)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import getGifs from "../services/getGifs";
 
 function App() {
-  const [value, updateValue] = useState([])
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(function (){
+    getGifs({keyWord: 'morty'}).then(gifs => setGifs(gifs))
+  }, [])
+
   return (
     <div className="App">
       <section className="App-content">
-        
+        {
+          gifs.map((singleGif) => (<img src={singleGif} />))
+        }
       </section>
     </div>
   );
